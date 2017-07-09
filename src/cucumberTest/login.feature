2ae3f7689f19e70d5e23c2a530d89eeaaa066372
Vishwakarma, Siyaram <Siyaram.Vishwakarma@cdk.com>
2017-07-09 15:45:07 +0530

    click on sign in is added

--- src/cucumberTest/login.feature
+++ src/cucumberTest/login.feature
@@ -3,11 +3,11 @@ Feature: Login Action
 @Login
 Scenario: Successful Login with Valid Credentials
 	Given User is on Home Page
-	When User Navigate to LogIn Page
-	And User enters UserName and Password
-	Then Message displayed Login Successfully
+	When User clicks on sign in link	
+#	And User enters UserName and Password
+#	Then Gmail inbox is displayed
 
-@Logout
-Scenario: Successful LogOut
-	When User LogOut from the Application
-	Then Message displayed LogOut Successfully
+#@Logout
+#Scenario: Successful LogOut
+#	When User LogOut from the Application
+#	Then Message displayed LogOut Successfully
--- src/stepDefs/Login.java
+++ src/stepDefs/Login.java
@@ -1,15 +1,27 @@
 package stepDefs;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.FindsByXPath;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 import cucumberTest.StartUp;
 
 public class Login {	
 	StartUp st = new StartUp();
-	WebDriver wd = st.getWebDriver("Chrome");
+	WebDriver driver = st.getWebDriver("Chrome");
+	WebElement sign_in = driver.findElement(By.xpath("html/body/nav/div/a[2]"));
+	
 	@Given("^User is on Home Page$")
 	public void User_is_on_Home_Page() {
-		wd.get("www.gmail.com");
+		driver.get("http://www.gmail.com");
+	}
+	@When("^User clicks on sign in link$")
+	public void user_clicks_on_signin_link(){
+		sign_in.click();
+	}	
+	public void User_enters_username_password(){		
 	}
 }
